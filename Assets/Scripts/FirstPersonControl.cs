637944fdcb87b4f65e3aa28231c7c1c493eb16bd
Alexis <alexis.morin.mail@gmail.com>
2020-01-25 21:45:25 -0500

    mvp

--- Assets/Scripts/FirstPersonControl.cs
+++ Assets/Scripts/FirstPersonControl.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPersonControl : MonoBehaviour
-{
+public class FirstPersonControl : MonoBehaviour {
 
     [SerializeField]
     Transform cameraObject;
@@ -15,10 +14,10 @@ public class FirstPersonControl : MonoBehaviour
     Vector2 _mouseAbsolute;
     Vector2 _smoothMouse;
 
-    public Vector2 clampInDegrees = new Vector2(360, 180);
+    public Vector2 clampInDegrees = new Vector2 (360, 180);
     public bool lockCursor;
-    public Vector2 sensitivity = new Vector2(2, 2);
-    public Vector2 smoothing = new Vector2(3, 3);
+    public Vector2 sensitivity = new Vector2 (2, 2);
+    public Vector2 smoothing = new Vector2 (3, 3);
     public Vector2 targetDirection;
     public Vector2 targetCharacterDirection;
 
@@ -30,68 +29,67 @@ public class FirstPersonControl : MonoBehaviour
 
     CharacterController characterController;
 
-    void Start()
-    {
+    void Start () {
         Cursor.lockState = CursorLockMode.Locked;
-        characterController = characterBody.GetComponent<CharacterController>();
+        characterController = characterBody.GetComponent<CharacterController> ();
     }
 
-    void Update()
-    {
+    void Update () {
         // Allow the script to clamp based on a desired target value.
         //     var targetOrientation = Quaternion.Euler (targetDirection);
         //    var targetCharacterOrientation = Quaternion.Euler (targetCharacterDirection);
 
         // Get raw mouse input for a cleaner reading on more sensitive mice.
-        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        var mouseDelta = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
 
         // Scale input against the sensitivity setting and multiply that against the smoothing value.
-        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+        mouseDelta = Vector2.Scale (mouseDelta, new Vector2 (sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
 
         // Interpolate mouse movement over time to apply smoothing delta.
-        _smoothMouse.x = Mathf.Lerp(_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
-        _smoothMouse.y = Mathf.Lerp(_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+        _smoothMouse.x = Mathf.Lerp (_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+        _smoothMouse.y = Mathf.Lerp (_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
 
         // Find the absolute mouse movement value from point zero.
         _mouseAbsolute += _smoothMouse;
 
         // Clamp and apply the local x value first, so as not to be affected by world transforms.
         if (clampInDegrees.x < 360)
-            _mouseAbsolute.x = Mathf.Clamp(_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
+            _mouseAbsolute.x = Mathf.Clamp (_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
 
         // Then clamp and apply the global y value.
         if (clampInDegrees.y < 360)
-            _mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
+            _mouseAbsolute.y = Mathf.Clamp (_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
 
-        transform.localRotation = Quaternion.AngleAxis(-_mouseAbsolute.y, Vector3.right);
+        transform.localRotation = Quaternion.AngleAxis (-_mouseAbsolute.y, Vector3.right);
 
         // If there's a character body that acts as a parent to the camera
-        if (characterBody)
-        {
-            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, Vector3.up);
+        if (characterBody) {
+            var yRotation = Quaternion.AngleAxis (_mouseAbsolute.x, Vector3.up);
             characterBody.transform.localRotation = yRotation;
-        }
-        else
-        {
-            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
+        } else {
+            var yRotation = Quaternion.AngleAxis (_mouseAbsolute.x, transform.InverseTransformDirection (Vector3.up));
             transform.localRotation *= yRotation;
         }
 
         Vector2 moveInput = Vector3.zero;
-        moveInput.x = Input.GetAxis("Horizontal");
-        moveInput.y = Input.GetAxis("Vertical");
+        moveInput.x = Input.GetAxis ("Horizontal");
+        moveInput.y = Input.GetAxis ("Vertical");
 
-        rig.SetFloat("x", moveInput.x);
-        rig.SetFloat("y", moveInput.y);
+        rig.SetFloat ("x", moveInput.x);
+        rig.SetFloat ("y", moveInput.y);
+
+        Vector3 relativeVelocity = transform.InverseTransformDirection (characterController.velocity);
+        print (relativeVelocity);
+        rig.SetFloat ("velX", relativeVelocity.x / 5f);
+        rig.SetFloat ("velY", relativeVelocity.z / 5f);
 
         Vector3 moveVector = moveInput.x * transform.right + moveInput.y * transform.forward;
 
-        characterController.SimpleMove(moveVector * moveSpeed);
+        characterController.SimpleMove (moveVector * moveSpeed);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown (KeyCode.Space)) {
             i++;
-            ScreenCapture.CaptureScreenshot(Application.dataPath + "/../Image" + i.ToString() + ".png");
+            ScreenCapture.CaptureScreenshot (Application.dataPath + "/../Image" + i.ToString () + ".png");
         }
 
     }
--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Teleporter : MonoBehaviour
-{
+public class Teleporter : MonoBehaviour {
 
     public Transform ghost;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            ghost.transform.position = this.transform.parent.position;
+    void OnTriggerEnter (Collider other) {
+        if (other.gameObject.tag == "Player") {
+            ghost.gameObject.GetComponent<NavMeshAgent> ().enabled = false;
+            ghost.gameObject.GetComponent<NavMeshAgent> ().nextPosition = transform.parent.position + Vector3.up * 2f;
+            ghost.transform.position = transform.parent.position;
+            ghost.gameObject.GetComponent<NavMeshAgent> ().enabled = true;
+
         }
 
     }
